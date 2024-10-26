19e2ae643eea12e74d2d73b3f62ee536384ae8b3
HeyAdin <adinahmad003@gmail.com>
2024-10-27 05:10:33 +0530

    Added todo schema

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,23 @@ require('dotenv').config();
 mongoose.connect(process.env.mongoURL);
 
 const userSchema = new mongoose.Schema({
-    username : String,
-    email : String,
-    pass : String
+    username: String,
+    email: String,
+    pass: String
 })
 
-const Users = mongoose.model('users',userSchema);
+const todosSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "users"
+    }]
+});
+
+const Todos = mongoose.model('todos', todosSchema);
+const Users = mongoose.model('users', userSchema);
 module.exports = {
-    Users
+    Users,
+    Todos
 }
