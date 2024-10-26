9484d4a720cc76ab302b6ac41dd13c9ff1799b98
HeyAdin <adinahmad003@gmail.com>
2024-10-27 00:08:02 +0530

    separated user validation logic

--- backend/index.js
+++ backend/index.js
@@ -8,42 +8,7 @@ const { Users } = require('./db');
 app.use(express.json());
 
 // new users input validation
-function validNewUserInput(req, res, next) {
-    const username = req.body.username;
-    const email = req.body.email;
-    const pass = req.body.pass;
-    const response = newUserSchema.safeParse({
-        username,
-        email,
-        pass
-    })
-    if (response.success) {
-        next();
-    }
-    else {
-        res.status(403).send("invalid input");
-    }
-}
-
-// existing user input valid
-function validUserInput(req, res, next) {
-    const email = req.body.email;
-    const username = req.body.username;
-    const pass = req.body.pass;
-    console.log(username,email,pass);
-    const response = userSchema.safeParse({
-        email,
-        pass
-    })
-    console.log(response.success);
-    if (response.success) {
-        next();
-    }
-    else {
-        res.status(403).send("invalid input");
-    }
-}
-
+const {validNewUserInput, validUserInput} = require('./validateUser')
 // user exist in database
 async function userExist(email,pass) {
     const exist = await Users.findOne({
---
+++ backend/validateUser.js
@@ -0,0 +1,43 @@
+const { newUserSchema, userSchema } = require('./types');
+
+
+function validNewUserInput(req, res, next) {
+    const username = req.body.username;
+    const email = req.body.email;
+    const pass = req.body.pass;
+    const response = newUserSchema.safeParse({
+        username,
+        email,
+        pass
+    })
+    if (response.success) {
+        next();
+    }
+    else {
+        res.status(403).send("invalid input");
+    }
+}
+
+// existing user input valid
+function validUserInput(req, res, next) {
+    const email = req.body.email;
+    const username = req.body.username;
+    const pass = req.body.pass;
+    console.log(username,email,pass);
+    const response = userSchema.safeParse({
+        email,
+        pass
+    })
+    console.log(response.success);
+    if (response.success) {
+        next();
+    }
+    else {
+        res.status(403).send("invalid input");
+    }
+}
+
+module.exports = {
+    validNewUserInput,
+    validUserInput
+} 
